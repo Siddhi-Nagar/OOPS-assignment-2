ffa5751ba309c39e262c747e99daafef3caba811
Siddhi-Nagar <nagarsiddhi11@gmail.com>
2024-11-24 19:28:59 +0530

    Create example.java

---
+++ example.java
@@ -0,0 +1,12 @@
+class Example {
+    public Example() {
+        System.out.println("This is a 0-argument constructor.");
+    }
+}
+
+public class example{
+    public static void main(String[] args) {
+        Example obj = new Example(); 
+    }
+}
+ 
